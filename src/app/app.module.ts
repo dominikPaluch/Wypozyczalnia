4b0e5546a9cd1337b713b61a6c1c9f9674d9c018
Dominik Paluch <dominik-paluch@o2.pl>
2017-05-24 15:50:15 +0200

    Add smart data table with sample data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
-import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './navigation/header/header.component';
-import { LeftMenuComponent } from './navigation/left-menu/left-menu.component';
-import { StartComponent } from './pages/start/start.component';
-import { FooterComponent } from './navigation/footer/footer.component';
-import { CarouselComponent } from './pages/carousel/carousel.component';
-import { EquipmentsComponent } from './pages/equipments/equipments.component';
-import { EquipmentService} from './equipment.service';
-import { EquipmentDetailComponent } from './pages/equipment-detail/equipment-detail.component';
-import { ContentComponent } from './navigation/content/content.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './navigation/header/header.component';
+import {LeftMenuComponent} from './navigation/left-menu/left-menu.component';
+import {StartComponent} from './pages/start/start.component';
+import {FooterComponent} from './navigation/footer/footer.component';
+import {CarouselComponent} from './pages/carousel/carousel.component';
+import {EquipmentsComponent} from './pages/equipments/equipments.component';
+import {EquipmentService} from './equipment.service';
+import {EquipmentDetailComponent} from './pages/equipment-detail/equipment-detail.component';
+import {ContentComponent} from './navigation/content/content.component';
+import { TableComponent } from './pages/table/table.component';
+
+import { DataTableModule } from 'angular-2-data-table';
+
 
 
 @NgModule({
@@ -31,7 +36,9 @@ import { ContentComponent } from './navigation/content/content.component';
     CarouselComponent,
     EquipmentsComponent,
     EquipmentDetailComponent,
-    ContentComponent
+    ContentComponent,
+    TableComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -39,8 +46,13 @@ import { ContentComponent } from './navigation/content/content.component';
     HttpModule,
     AppRoutingModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
+    DataTableModule
+
   ],
-  providers: [EquipmentService],
+  providers: [
+    EquipmentService,
+    ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
