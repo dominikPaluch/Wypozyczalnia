a008cd7d647cedae4437da365c508b8edbe9c76e
Dominik Paluch <dominik-paluch@o2.pl>
2017-05-24 18:44:38 +0200

    Add image to data table

--- src/app/pages/table/table-data.ts
+++ src/app/pages/table/table-data.ts
@@ -1,6 +1,22 @@
-export var cars = [
-  { year: 1997, maker: 'Ford', model: 'E350', desc: 'ac, abs, moon', price: 3000.00 },
-  { year: 1999, maker: 'Chevy', model: 'Venture "Extended Edition"', price: 4900.00 },
-  { year: 1999, maker: 'Checy', model: 'Venture "Extended Edition, Very Large"', price: 5000.00 },
-  { year: 1996, maker: 'Jeep', model: 'Grand Cherokee', desc: 'air, moon roof, loaded', price: 4799.00 }
+// export var cars = [
+//   { year: 1997, maker: 'Ford', model: 'E350', desc: 'ac, abs, moon', price: 3000.00 },
+//   { year: 1999, maker: 'Chevy', model: 'Venture "Extended Edition"', price: 4900.00 },
+//   { year: 1999, maker: 'Checy', model: 'Venture "Extended Edition, Very Large"', price: 5000.00 },
+//   { year: 1996, maker: 'Jeep', model: 'Grand Cherokee', desc: 'air, moon roof, loaded', price: 4799.00 }
+// ];
+
+export var equipments = [
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty1', size: 185, desc: 'ac, abs, moon', price: 30.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty2', size: 170, price: 300.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty3', size: 155, price: 500.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty4', size: 196, price: 300.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty5', size: 200, price: 30.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty6', size: 188, price: 3000.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty1', size: 185, price: 3000.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty2', size: 170, price: 3000.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty3', size: 155, price: 3000.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty4', size: 196, price: 3000.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty5', size: 200, price: 3000.00 },
+  { url: 'http://lorempixel.com/100/100/sports/', name: 'Narty6', size: 188, price: 3000.00 },
+
 ];
